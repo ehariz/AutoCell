d2348502b3e4e0485918bc4f1a64fc655da38def
Maxime <maxime.escourrou@etu.utc.fr>
2018-04-09 16:29:19 +0200

    Chargement des voisins dans CellHandler

--- AutoCell/cellhandler.cpp
+++ AutoCell/cellhandler.cpp
@@ -45,6 +45,8 @@ CellHandler::CellHandler(QString filename)
         throw QString(QObject::tr("File not valid"));
     }
 
+    foundNeighbours();
+
 }
 
 /** \fn CellHandler::~CellHandler()
@@ -57,6 +59,15 @@ CellHandler::~CellHandler()
         delete it.value();
     }
 }
+
+/** \fn Cell *CellHandler::getCell(const QVector<unsigned int> position) const
+ * \brief Access the cell to the given position
+ */
+Cell *CellHandler::getCell(const QVector<unsigned int> position) const
+{
+    return m_cells.value(position);
+}
+
 /** \fn bool CellHandler::load(const QJsonObject &json)
  * \brief Load the config file in the CellHandler
  *
@@ -65,23 +76,23 @@ CellHandler::~CellHandler()
  * position.clear();
  * for (unsigned short i = 0; i < m_dimensions.size(); i++)
  * {
- *  position.push_back(0);
+ *      position.push_back(0);
  * }
  * for (unsigned int j = 0; j < m_cells.size(); j++)
  * {
- *  std::cout << m_cells.value(position)->getState() << " ";
- *  position.replace(0, position.at(0)+1);
- *  for (unsigned short i = 0; i < m_dimensions.size(); i++)
- *  {
- *      if (position.at(i) >= m_dimensions.at(i))
+ *      std::cout << m_cells.value(position)->getState() << " ";
+ *      position.replace(0, position.at(0)+1);
+ *      for (unsigned short i = 0; i < m_dimensions.size(); i++)
  *      {
- *          position.replace(i, 0);
- *          std::cout << std::endl;
- *          if (i + 1 != m_dimensions.size())
- *              position.replace(i+1, position.at(i+1)+1);
- *      }
+ *          if (position.at(i) >= m_dimensions.at(i))
+ *          {
+ *              position.replace(i, 0);
+ *              std::cout << std::endl;
+ *              if (i + 1 != m_dimensions.size())
+ *                  position.replace(i+1, position.at(i+1)+1);
+ *          }
  *
- *  }
+ *      }
  * }
  * \endcode
  *
@@ -134,20 +145,153 @@ bool CellHandler::load(const QJsonObject &json)
             return false;
         m_cells.insert(position, new Cell(cells.at(j).toDouble()));
 
-        position.replace(0, position.at(0)+1);
-        for (unsigned short i = 0; i < m_dimensions.size(); i++)
-        {
-            if (position.at(i) >= m_dimensions.at(i))
-            {
-                position.replace(i, 0);
-                if (i + 1 != m_dimensions.size())
-                    position.replace(i+1, position.at(i+1)+1);
-            }
-
-        }
+        positionIncrement(position);
     }
 
-
     return true;
+
+}
+
+/** \fn bool CellHandler::foundNeighbours()
+ * \brief Set the neighbours of each cells
+ *
+ * Careful, this is in O(n*3^d), with n the number of cells and d the number of dimensions
+ *
+ */
+void CellHandler::foundNeighbours()
+{
+    QVector<unsigned int> currentPosition;
+    // Set position vector to 0
+    for (unsigned short i = 0; i < m_dimensions.size(); i++)
+    {
+        currentPosition.push_back(0);
+    }
+    // Modification of all the cells
+    for (unsigned int j = 0; j < m_cells.size(); j++)
+    {
+        // Get the list of the neighbours positions
+        // This function is recursive
+        QVector<QVector<unsigned int> > listPosition(getListNeighboursPositions(currentPosition));
+
+        // Adding neighbours
+        for (unsigned int i = 0; i < listPosition.size(); i++)
+            m_cells.value(currentPosition)->addNeighbour(m_cells.value(listPosition.at(i)));
+
+        positionIncrement(currentPosition);
+    }
+}
+
+/** \fn void CellHandler::positionIncrement(QVector<unsigned int> &pos, unsigned int value) const
+ * \brief Increment the QVector given by the value choosen
+ *
+ * Careful, when the position reach the maximum, it goes to zero without leaving the function
+ *
+ * \param pos Position to increment
+ * \param value Value to add, 1 by default
+ */
+void CellHandler::positionIncrement(QVector<unsigned int> &pos, unsigned int value) const
+{
+    pos.replace(0, pos.at(0) + value); // adding the value to the first digit
+
+    // Carry management
+    for (unsigned short i = 0; i < m_dimensions.size(); i++)
+    {
+        if (pos.at(i) >= m_dimensions.at(i) && pos.at(i) < m_dimensions.at(i)*2)
+        {
+            pos.replace(i, 0);
+            if (i + 1 != m_dimensions.size())
+                pos.replace(i+1, pos.at(i+1)+1);
+        }
+        else if (pos.at(i) >= m_dimensions.at(i))
+        {
+            pos.replace(i, pos.at(i) - m_dimensions.at(i));
+            if (i + 1 != m_dimensions.size())
+                pos.replace(i+1, pos.at(i+1)+1);
+            i--;
+        }
+
+    }
+}
+
+/** \fn QVector<QVector<unsigned int> >& CellHandler::getListNeighboursPositions(const QVector<unsigned int> position) const
+ * \brief Prepare the call of the recursive version of itself
+ *
+ * \param position Position of the central cell (x1,x2,x3,..,xn)
+ * \return List of positions
+ */
+QVector<QVector<unsigned int> >& CellHandler::getListNeighboursPositions(const QVector<unsigned int> position) const
+{
+    QVector<QVector<unsigned int> > *list = getListNeighboursPositionsRecursive(position, position.size(), position);
+    // We remove the position of the cell
+    list->removeAll(position);
+    return *list;
+}
+
+/** \fn QVector<QVector<unsigned int> >* CellHandler::getListNeighboursPositionsRecursive(const QVector<unsigned int> position, unsigned int dimension, QVector<unsigned int> lastAdd) const
+ * \brief Recursive function which browse the position possibilities tree
+ *
+ * Careful, the complexity is in O(3^dimension)<br>
+ * Piece of the tree:
+ *```
+ *             x_d -1
+ *           /
+ * x_(d-1)-1/_ x_d
+ *          \
+ *           \
+ *             x_d +1
+ *
+ *             x_d -1
+ *           /
+ * x_(d-1)  /_ x_d
+ *          \
+ *           \
+ *             x_d +1
+ *
+ *             x_d -1
+ *           /
+ * x_(d-1)+1/ x_d
+ *          \
+ *           \
+ *             x_d +1
+ *```
+ * The path in the tree to reach the leaf give the position
+ *
+ * \param position Position of the cell
+ * \param dimension Current working dimension (number of the digit). Dimension = 2 <=> working on x2 coordinates on (x1, x2, x3, ..., xn) vector
+ * \param lastAdd Last position added. Like the father node of the new tree
+ * \return List of position
+ */
+QVector<QVector<unsigned int> >* CellHandler::getListNeighboursPositionsRecursive(const QVector<unsigned int> position, unsigned int dimension, QVector<unsigned int> lastAdd) const
+{
+    if (dimension == 0) // Stop condition
+    {
+        QVector<QVector<unsigned int> > *list = new QVector<QVector<unsigned int> >;
+        return list;
+    }
+    QVector<QVector<unsigned int> > *listPositions = new QVector<QVector<unsigned int> >;
+
+    QVector<unsigned int> modifiedPosition(lastAdd);
+
+    // "x_d - 1" tree
+    if (modifiedPosition.at(dimension-1) != 0) // Avoid "negative" position
+        modifiedPosition.replace(dimension-1, position.at(dimension-1) - 1);
+    listPositions->append(*getListNeighboursPositionsRecursive(position, dimension -1, modifiedPosition));
+    if (!listPositions->count(modifiedPosition))
+        listPositions->push_back(modifiedPosition);
+
+    // "x_d" tree
+    modifiedPosition.replace(dimension-1, position.at(dimension-1));
+    listPositions->append(*getListNeighboursPositionsRecursive(position, dimension -1, modifiedPosition));
+    if (!listPositions->count(modifiedPosition))
+        listPositions->push_back(modifiedPosition);
+
+    // "x_d + 1" tree
+    if (modifiedPosition.at(dimension -1) + 1 < m_dimensions.at(dimension-1)) // Avoid position out of the cell space
+        modifiedPosition.replace(dimension-1, position.at(dimension-1) +1);
+    listPositions->append(*getListNeighboursPositionsRecursive(position, dimension -1, modifiedPosition));
+    if (!listPositions->count(modifiedPosition))
+        listPositions->push_back(modifiedPosition);
+
+    return listPositions;
 
 }
--- AutoCell/cellhandler.h
+++ AutoCell/cellhandler.h
@@ -16,8 +16,14 @@ public:
     CellHandler(QString filename);
     virtual ~CellHandler();
 
+    Cell* getCell(const QVector<unsigned int> position) const;
+
 private:
     bool load(const QJsonObject &json);
+    void foundNeighbours();
+    void positionIncrement(QVector<unsigned int> &pos, unsigned int value = 1) const;
+    QVector<QVector<unsigned int> > *getListNeighboursPositionsRecursive(const QVector<unsigned int> position, unsigned int dimension, QVector<unsigned int> lastAdd) const;
+    QVector<QVector<unsigned int> > &getListNeighboursPositions(const QVector<unsigned int> position) const;
 
     QVector<unsigned int> m_dimensions; ///< Vector of x dimensions
     QMap<QVector<unsigned int>, Cell* > m_cells; ///< Map of cells, with a x dimensions vector as key
