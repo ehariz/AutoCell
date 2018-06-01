e9c592b3875ffb20f3172f8ff856f444cc902462
harizeli <harizeli@etu.utc.fr>
2018-06-01 17:21:37 +0200

    Added tabs

--- AutoCell/mainwindow.cpp
+++ AutoCell/mainwindow.cpp
@@ -5,13 +5,12 @@ MainWindow::MainWindow(QWidget *parent) : QMainWindow(parent)
     createIcons();
     createActions();
     createToolBar();
-    createBoard();
 
 
     setMinimumSize(500,500);
     setWindowTitle("AutoCell");
 
-    m_cellHandler = NULL;
+    m_tabs = NULL;
 }
 
 /** \fn MainWindow::createIcons()
@@ -60,13 +59,13 @@ void MainWindow::createActions(){
 
 
 
-    m_fastBackwardBt = new QToolButton();
-    m_fastForwardBt = new QToolButton();
-    m_playPauseBt = new QToolButton();
-    m_saveAutomatonBt = new QToolButton();
-    m_newAutomatonBt = new QToolButton();
-    m_openAutomatonBt = new QToolButton();
-    m_resetBt = new QToolButton();
+    m_fastBackwardBt = new QToolButton(this);
+    m_fastForwardBt = new QToolButton(this);
+    m_playPauseBt = new QToolButton(this);
+    m_saveAutomatonBt = new QToolButton(this);
+    m_newAutomatonBt = new QToolButton(this);
+    m_openAutomatonBt = new QToolButton(this);
+    m_resetBt = new QToolButton(this);
 
     m_fastBackwardBt->setDefaultAction(m_fastBackward);
     m_fastForwardBt->setDefaultAction(m_fastForward);
@@ -97,7 +96,7 @@ void MainWindow::createActions(){
 
 void MainWindow::createToolBar(){
     m_toolBar = new QToolBar(this);
-    QLabel *m_speedLabel = new QLabel(tr("Speed : "));
+    QLabel *m_speedLabel = new QLabel(tr("Speed : "),this);
     m_jumpSpeed = new QSpinBox(this);
     m_jumpSpeed->setValue(1);
     m_speedLabel->setFixedWidth(50);
@@ -117,7 +116,7 @@ void MainWindow::createToolBar(){
 
 
     tbLayout->setAlignment(Qt::AlignCenter);
-    QWidget* wrapper = new QWidget();
+    QWidget* wrapper = new QWidget(this);
     wrapper->setLayout(tbLayout);
     m_toolBar->addWidget(wrapper);
     addToolBar(m_toolBar);
@@ -125,34 +124,51 @@ void MainWindow::createToolBar(){
 
 }
 
-/** \fn MainWindow::createBoard()
- * \brief Creates the Automaton board
+/** \fn MainWindow::createTab()
+ * \brief Creates a new Tab with an empty board
  */
 
-void MainWindow::createBoard(){
-    m_Board = new QTableWidget(m_boardVSize, m_boardHSize, this);
-        m_Board->setFixedSize(m_boardHSize*m_cellSize,m_boardVSize*m_cellSize);
+QWidget* MainWindow::createTab(){
+    QWidget *tab = new QWidget(this);
+    QVBoxLayout *layout = new QVBoxLayout(this);
+
+    QVector<unsigned int> dimensions = m_cellHandlers.last()->getDimensions();
+    int boardVSize = 0;
+    int boardHSize = 0;
+    if(dimensions.size() > 1){
+        boardVSize = dimensions[0];
+        boardHSize = dimensions[1];
+    }
+    else{
+        boardVSize = 1;
+        boardHSize = dimensions[0];
+    }
+
+    QTableWidget* board = new QTableWidget(boardVSize, boardHSize, this);
+        board->setFixedSize(boardHSize*m_cellSize,boardVSize*m_cellSize);
         //setMinimumSize(m_boardHSize*m_cellSize,100+m_boardVSize*m_cellSize);
-        m_Board->horizontalHeader()->setVisible(false);
-        m_Board->verticalHeader()->setVisible(false);
-        m_Board->setVerticalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
-        m_Board->setHorizontalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
-        m_Board->setEditTriggers(QAbstractItemView::NoEditTriggers);
-        for(unsigned int col = 0; col < m_boardHSize; ++col)
-            m_Board->setColumnWidth(col, m_cellSize);
-        for(unsigned int row = 0; row < m_boardVSize; ++row) {
-            m_Board->setRowHeight(row, m_cellSize);
-            for(unsigned int col = 0; col < m_boardHSize; ++col) {
-                m_Board->setItem(row, col, new QTableWidgetItem(""));
-                m_Board->item(row, col)->setBackgroundColor("white");
-                m_Board->item(row, col)->setTextColor("black");
+        board->horizontalHeader()->setVisible(false);
+        board->verticalHeader()->setVisible(false);
+        board->setVerticalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
+        board->setHorizontalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
+        board->setEditTriggers(QAbstractItemView::NoEditTriggers);
+        for(unsigned int col = 0; col < boardHSize; ++col)
+            board->setColumnWidth(col, m_cellSize);
+        for(unsigned int row = 0; row < boardVSize; ++row) {
+            board->setRowHeight(row, m_cellSize);
+            for(unsigned int col = 0; col < boardHSize; ++col) {
+                board->setItem(row, col, new QTableWidgetItem(""));
+                board->item(row, col)->setBackgroundColor("white");
+                board->item(row, col)->setTextColor("black");
             }
         }
      QScrollArea *scrollArea = new QScrollArea(this);
-     scrollArea->setWidget(m_Board);
-     setCentralWidget(scrollArea);
+     scrollArea->setWidget(board);
+     layout->setContentsMargins(0,0,0,0);
+     layout->addWidget(scrollArea);
+     tab->setLayout(layout);
+     return tab;
 }
-
 
 /** \fn MainWindow::openFile()
  * \brief Opens a file browser for the user to select automaton files and creates an automaton
@@ -161,18 +177,11 @@ void MainWindow::openFile(){
     QString fileName = QFileDialog::getOpenFileName(this, tr("Open Cell file"), ".",
                                                     tr("Automaton cell files (*.atc)"));
     if(!fileName.isEmpty()){
-        m_cellHandler = new CellHandler(fileName);
-        QVector<unsigned int> dimensions = m_cellHandler->getDimensions();
-        if(dimensions.size() > 1){
-            m_boardVSize = dimensions[0];
-            m_boardHSize = dimensions[1];
-        }
-        else{
-            m_boardVSize = 1;
-            m_boardHSize = dimensions[0];
-        }
-        createBoard();
-        updateBoard();
+        m_cellHandlers.append(new CellHandler(fileName));
+        std::cout << "m_cellHandlers size :" <<m_cellHandlers.size() << std::endl<<std::flush;
+        if(m_tabs == NULL) createTabs();
+        m_tabs->addTab(createTab(), "Automaton "+ QString::number(m_cellHandlers.size()));
+        updateBoard(m_cellHandlers.size()-1);
     }
 }
 
@@ -181,10 +190,10 @@ void MainWindow::openFile(){
  * \brief Allows user to select a location and saves automaton's state and settings
  */
 void MainWindow::saveToFile(){
-    if(m_cellHandler != NULL){
+    if(m_cellHandlers.size() > 0){
         QString fileName = QFileDialog::getSaveFileName(this, tr("Save Automaton"),
                                                         ".", tr("Automaton Cells file (*.atc"));
-        m_cellHandler->save(fileName+".atc");
+        m_cellHandlers[m_tabs->currentIndex()]->save(fileName+".atc");
 
     }
     else{
@@ -214,17 +223,17 @@ void MainWindow::openCreationWindow(){
 void MainWindow::setCellHandler(const QVector<unsigned int> dimensions,
                                 CellHandler::generationTypes type,
                                 unsigned int stateMax, unsigned int density){
-    m_cellHandler = new CellHandler(dimensions, type, stateMax, density);
-    if(dimensions.size() > 1){
-        m_boardVSize = dimensions[0];
-        m_boardHSize = dimensions[1];
-    }
-    else{
-        m_boardVSize = 1;
-        m_boardHSize = dimensions[0];
-    }
-    createBoard();
-    updateBoard();
+    CellHandler* newCellHandler = new CellHandler(dimensions, type, stateMax, density);
+
+    if(m_tabs == NULL) createTabs();
+
+    m_cellHandlers.append(newCellHandler);
+    std::cout << "m_cellHandlers size :" <<m_cellHandlers.size() << std::endl<<std::flush;
+    QWidget* newTab = createTab();
+    m_tabs->addTab(newTab, "Automaton "+ QString::number(m_cellHandlers.size()));
+    m_tabs->setCurrentWidget(newTab);
+    updateBoard(m_cellHandlers.size()-1);
+
 }
 
 /** \fn MainWindow::nextState(int n)
@@ -232,39 +241,54 @@ void MainWindow::setCellHandler(const QVector<unsigned int> dimensions,
  */
 
 void MainWindow::nextState(int n){
-    if(m_cellHandler == NULL){
+    if(m_cellHandlers.size() == 0){
         QMessageBox msgBox;
         msgBox.critical(0,"Error","Please create or import an Automaton first !");
         msgBox.setFixedSize(500,200);
     }
     else{
-        for(unsigned int i = 0; i < n; i++) m_cellHandler->nextStates();
-        updateBoard();
+        for(unsigned int i = 0; i < n; i++) m_cellHandlers[m_tabs->currentIndex()]->nextStates();
+        updateBoard(m_tabs->currentIndex());
     }
 }
 
 /** \fn MainWindow::updateBoard()
- * \brief Updates cells on the board with the cellHandler's cells states
+ * \brief Updates cells on the board on the tab at the give index with the cellHandler's cells states
  */
 
-void MainWindow::updateBoard(){
-    if(m_cellHandler == NULL){
+void MainWindow::updateBoard(int index){
+    if(m_cellHandlers.size()==0){
         QMessageBox msgBox;
         msgBox.critical(0,"Error","Please create or import an Automaton first !");
         msgBox.setFixedSize(500,200);
     }
     else{
-        int i = 0;
-        int j = 0;
-        for (CellHandler::iterator it = m_cellHandler->begin(); it != m_cellHandler->end() && it.changedDimension() < 2; ++it){
-                if(it.changedDimension() > 0){
-                    i = 0;
-                    j++;
-                    std::cout << std::endl;
-                }
-                m_Board->item(i,j)->setText(QString::number(it->getState()));
-                i++;
+
+        CellHandler* cellHandler = m_cellHandlers[index];
+        QVector<unsigned int> dimensions = cellHandler->getDimensions();
+        QTableWidget* board = getBoard(index);
+        if(dimensions.size() > 1){
+            int i = 0;
+            int j = 0;
+            for (CellHandler::const_iterator it = CellHandler::const_iterator(cellHandler); it != cellHandler->end() && it.changedDimension() < 2; ++it){
+                    if(it.changedDimension() > 0){
+                        i = 0;
+                        j++;
+                        std::cout << std::endl;
+                    }
+                    board->item(i,j)->setText(QString::number(it->getState()));
+                    i++;
+            }
         }
+        else{
+            int i = 0;
+            int j = 0;
+            for (CellHandler::const_iterator it = CellHandler::const_iterator(cellHandler); it != cellHandler->end() && it.changedDimension() < 1; ++it){
+                    board->item(i,j)->setText(QString::number(it->getState()));
+                    j++;
+            }
+        }
+
     }
 
 }
@@ -275,4 +299,30 @@ void MainWindow::updateBoard(){
 
 void MainWindow::forward(){
     nextState(m_jumpSpeed->value());
+}
+
+QTableWidget* MainWindow::getBoard(int n){
+    return m_tabs->widget(n)->findChild<QTableWidget *>();
+}
+
+/** \fn MainWindow::createTabs()
+ * \brief Creates a QTabWidget for the main window and displays it
+ */
+
+void MainWindow::createTabs(){
+    m_tabs = new QTabWidget(this);
+    m_tabs->setMovable(true);
+    m_tabs->setTabsClosable(true);
+    setCentralWidget(m_tabs);
+    connect(m_tabs, SIGNAL(tabCloseRequested(int)), this, SLOT(closeTab(int)));
+}
+
+/** \fn MainWindow::closeTab(int n)
+ * \brief Closes the tab at index n. Before closing, prompts the user to save the automaton
+ */
+
+void MainWindow::closeTab(int n){
+    m_tabs->setCurrentIndex(n);
+    saveToFile();
+    m_tabs->removeTab(n);
 }
